addb1a5e043d297dfda72802a580a8481949555d
이름 <>
2020-04-05 17:36:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/coronatravel/HttpReqTask.java
@@ -0,0 +1,49 @@
+package com.example.coronatravel;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpReqTask extends AsyncTask <String,String,String>{
+    protected String doInBackground(String... arg) {
+        String response = getHttpConnResult(arg[0]);
+        return response;
+    }
+
+    protected void onPostExecute(String result) {
+
+    }
+
+
+
+    public String getHttpConnResult(String strUrl) {
+        String line;
+        String result = new String();
+        try {
+            URL url = new URL(strUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setReadTimeout(5000);
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+            while((line = reader.readLine()) != null) {
+                result += line + '\n';
+                if( result.length() > 200 ) break;
+            }
+            reader.close();
+            conn.disconnect();
+        } catch(Exception e) {
+            Log.d("tag", "HttpURLConnection error");
+        }
+        return result;
+    }
+}
