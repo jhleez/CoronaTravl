364568186a97f0f4c4dd948c52bbd87fc29914ad
JunBong <yg02005@naver.com>
2020-05-01 18:15:24 +0900

    통합검색 Spinner 수정완료

--- app/src/main/java/com/example/coronatravel/ui/totalsearch/TotalsearchFragment.java
+++ app/src/main/java/com/example/coronatravel/ui/totalsearch/TotalsearchFragment.java
@@ -20,7 +20,7 @@ import androidx.viewpager.widget.ViewPager;
 import com.example.coronatravel.R;
 import com.example.coronatravel.Adapter.SwipeAdapter;
 
-public class TotalsearchFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+public class TotalsearchFragment extends Fragment  {
 
     private TotalsearchViewModel totalsearchViewModel;
     int searchtype;
@@ -47,11 +47,92 @@ public class TotalsearchFragment extends Fragment implements AdapterView.OnItemS
 
         spinner_hightype = root.findViewById(R.id.spinner_totalsearch_hightype);
         spinner_middletype = root.findViewById(R.id.spinner_totalsearch_middletype);
-        spinner_hightype.setOnItemSelectedListener(this);
+        spinner_hightype.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] model_1 = null;
+                if (position == 0) {
+                    model_1 = getResources().getStringArray(R.array.middletype);
+                } else if (position == 1) {
+                    model_1 = getResources().getStringArray(R.array.middle_1);
+                } else if (position == 2) {
+                    model_1 = getResources().getStringArray(R.array.middle_2);
+                } else if (position == 3) {
+                    model_1 = getResources().getStringArray(R.array.middle_3);
+                } else if (position == 4) {
+                    model_1 = getResources().getStringArray(R.array.middle_4);
+                } else if (position == 5) {
+                    model_1 = getResources().getStringArray(R.array.middle_5);
+                } else if (position == 6) {
+                    model_1 = getResources().getStringArray(R.array.middle_6);
+                } else if (position == 7) {
+                    model_1 = getResources().getStringArray(R.array.middle_7);
+                }
+                ArrayAdapter<String> adapter_1 = new ArrayAdapter<String>(getContext(), R.layout.support_simple_spinner_dropdown_item, model_1);
+                adapter_1.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+                spinner_middletype.setAdapter(adapter_1);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         spinner_bigcity = root.findViewById(R.id.spinner_totalsearch_bigcity);
         spinner_smallcity = root.findViewById(R.id.spinner_totalsearch_smallcity);
+        spinner_bigcity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] model_2 = null;
+                if (position == 0) {
+                    model_2 = getResources().getStringArray(R.array.total_middle);
+                } else if (position == 1) {
+                    model_2 = getResources().getStringArray(R.array.seoul_middle);
+                } else if (position == 2) {
+                    model_2 = getResources().getStringArray(R.array.incheon_middle);
+                } else if (position == 3) {
+                    model_2 = getResources().getStringArray(R.array.daejeun_middle);
+                } else if (position == 4) {
+                    model_2 = getResources().getStringArray(R.array.daegu_middle);
+                } else if (position == 5) {
+                    model_2 = getResources().getStringArray(R.array.gangju_middle);
+                } else if (position == 6) {
+                    model_2 = getResources().getStringArray(R.array.busan_middle);
+                } else if (position == 7) {
+                    model_2 = getResources().getStringArray(R.array.ulsan_middle);
+                } else if (position == 8) {
+                    model_2 = getResources().getStringArray(R.array.sejong_middle);
+                } else if (position == 9) {
+                    model_2 = getResources().getStringArray(R.array.geunggi_middle);
+                } else if (position == 10) {
+                    model_2 = getResources().getStringArray(R.array.gangwon_middle);
+                } else if (position == 12) {
+                    model_2 = getResources().getStringArray(R.array.choongbook_middle);
+                } else if (position == 13) {
+                    model_2 = getResources().getStringArray(R.array.choongnam_middle);
+                } else if (position == 14) {
+                    model_2 = getResources().getStringArray(R.array.geungbook_middle);
+                } else if (position == 15) {
+                    model_2 = getResources().getStringArray(R.array.geungnak_middle);
+                } else if (position == 16) {
+                    model_2 = getResources().getStringArray(R.array.junbook_middle);
+                } else if (position == 17) {
+                    model_2 = getResources().getStringArray(R.array.junnam_midlle);
+                } else if (position == 18) {
+                    model_2 = getResources().getStringArray(R.array.jeaju_middle);
+                }
+                ArrayAdapter<String> adapter_2 = new ArrayAdapter<String>(getContext(), R.layout.support_simple_spinner_dropdown_item, model_2);
+                adapter_2.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+                spinner_smallcity.setAdapter(adapter_2);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         spinner_searchtype = root.findViewById(R.id.spinner_totalsearch_searchtype);
 
@@ -100,44 +181,5 @@ public class TotalsearchFragment extends Fragment implements AdapterView.OnItemS
             return "39";
         }
         return "";
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-        String[] models = null;
-        if (position == 0) {
-            models = getResources().getStringArray(R.array.middletype);
-
-        } else if (position == 1) {
-            models = getResources().getStringArray(R.array.middle_1);
-
-        } else if (position == 2) {
-            models = getResources().getStringArray(R.array.middle_2);
-
-        } else if (position == 3) {
-            models = getResources().getStringArray(R.array.middle_3);
-
-        } else if (position == 4) {
-            models = getResources().getStringArray(R.array.middle_4);
-
-        } else if (position == 5) {
-            models = getResources().getStringArray(R.array.middle_5);
-
-        } else if (position == 6) {
-            models = getResources().getStringArray(R.array.middle_6);
-
-        } else if (position == 7) {
-            models = getResources().getStringArray(R.array.middle_7);
-
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (getContext(), R.layout.support_simple_spinner_dropdown_item, models);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        spinner_middletype.setAdapter(adapter);
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
     }
 }
