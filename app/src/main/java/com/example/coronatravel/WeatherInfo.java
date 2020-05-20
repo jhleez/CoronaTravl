8adf7f3f2d0741d68bdcf7b5a15c1ccb521e6ebc
JunBong <yg02005@naver.com>
2020-05-20 19:12:31 +0900

    수정필요한 부분

--- app/src/main/java/com/example/coronatravel/WeatherInfo.java
+++ app/src/main/java/com/example/coronatravel/WeatherInfo.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,17 +20,67 @@ public class WeatherInfo extends AppCompatActivity {
     TextView test;
     private RecyclerView weatherListview;
     private myWeatherAdapter weatherAdapter;
+    LinearLayoutManager layoutManager;
+    ArrayList<weatherListViewItem> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_info);
+        weatherListview = findViewById(R.id.weatherListview);
 
+        getdata();
+        init();
+    }
 
-        String pagenumber="1";
+    public void init() {
+
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        weatherListview.setLayoutManager(layoutManager);
+
+        itemList = new ArrayList<>();
+
+        for (int i = 0; i < MainActivity.ShortWeather_ArrayList.size(); i++) {
+            String rainState = MainActivity.ShortWeather_ArrayList.get(i).getPTY();
+            String skyState = MainActivity.ShortWeather_ArrayList.get(i).getSKY();
+            String month = MainActivity.ShortWeather_ArrayList.get(i).getFcstDate().substring(4, 6);
+            String date = MainActivity.ShortWeather_ArrayList.get(i).getFcstDate().substring(6);
+            Drawable skyIcon = null;
+            if (rainState.equals("1") || rainState.equals("4")) //Rain or shower
+                skyIcon = ContextCompat.getDrawable(this, R.drawable.rainicon);
+            else if (rainState.equals("2")) //Rain or Snow
+                skyIcon = ContextCompat.getDrawable(this, R.drawable.rainsnowicon);
+            else if (rainState.equals("3")) //Snow
+                skyIcon = ContextCompat.getDrawable(this, R.drawable.snowicon);
+                //else if(rainState.equals("4")) //Shower
+                //skyIcon = ContextCompat.getDrawable(this,R.drawable.showericon);
+            else if (rainState.equals("0")) { //nothing
+                if (skyState.equals("1")) //sunny
+                    skyIcon = ContextCompat.getDrawable(this, R.drawable.sunnyicon);
+                else if (skyState.equals("3")) //lots of cloud
+                    skyIcon = ContextCompat.getDrawable(this, R.drawable.lotsofcloudicon);
+                else if (skyState.equals("4")) //cloudy
+                    skyIcon = ContextCompat.getDrawable(this, R.drawable.cloudyicon);
+                else Toast.makeText(this, "skyState is " + skyState, Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(WeatherInfo.this, "rainState is " + rainState, Toast.LENGTH_SHORT).show();
+
+            itemList.add(new weatherListViewItem(skyIcon, month + "월 " + date + "일",
+                    MainActivity.ShortWeather_ArrayList.get(i).getTMN() + "/" + MainActivity.ShortWeather_ArrayList.get(i).getTMX()));
+        }
+
+        weatherAdapter = new myWeatherAdapter(this, itemList);
+        weatherListview.setAdapter(weatherAdapter);
+
+        myWeatherListviewDecoration decoration = new myWeatherListviewDecoration();
+        weatherListview.addItemDecoration(decoration);
+    }
+
+    public void getdata() {
+        String pagenumber = "1";
         String ShortWeatherURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=LuQHzrmd0D8xz9tDN8srTETgDoVfSeUV%2FAvFrhKX%2BtTdNMG7GJINi%2B6INCB7yMFJXXIO%2FKb7JfNeFdA%2BNmEIqA%3D%3D" +
                 "&numOfRows=50" +
-                "&pageNo=" + pagenumber+
+                "&pageNo=" + pagenumber +
                 "&dataType=JSON" +
                 "&base_date=20200520" +
                 "&base_time=0500" +
@@ -42,16 +93,14 @@ public class WeatherInfo extends AppCompatActivity {
         } catch (Exception e) {
             Log.d("TAG", "jsonparsing error");
         }
-        test = (TextView)findViewById(R.id.test);
-
         String totalCount = ShortWeather.JSONParsing(JSONFromShortWeatherURL);
         int totalPage = (Integer.parseInt(totalCount) / 50) + 1;
         pagenumber = String.valueOf(Integer.parseInt(pagenumber) + 1);
 
-        while (Integer.parseInt(pagenumber) <= totalPage){
+        while (Integer.parseInt(pagenumber) <= totalPage) {
             ShortWeatherURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=LuQHzrmd0D8xz9tDN8srTETgDoVfSeUV%2FAvFrhKX%2BtTdNMG7GJINi%2B6INCB7yMFJXXIO%2FKb7JfNeFdA%2BNmEIqA%3D%3D" +
                     "&numOfRows=50" +
-                    "&pageNo=" + pagenumber+
+                    "&pageNo=" + pagenumber +
                     "&dataType=JSON" +
                     "&base_date=20200520" +
                     "&base_time=0500" +
@@ -68,7 +117,7 @@ public class WeatherInfo extends AppCompatActivity {
 
         String ShortWeatherURL2 = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=LuQHzrmd0D8xz9tDN8srTETgDoVfSeUV%2FAvFrhKX%2BtTdNMG7GJINi%2B6INCB7yMFJXXIO%2FKb7JfNeFdA%2BNmEIqA%3D%3D" +
                 "&numOfRows=50" +
-                "&pageNo=" +"1"+
+                "&pageNo=" + "1" +
                 "&dataType=JSON" +
                 "&base_date=20200520" +
                 "&base_time=0200" +
@@ -84,58 +133,12 @@ public class WeatherInfo extends AppCompatActivity {
 
         String a = ShortWeather.JSONParsing2(JSONFromShortWeatherURL2);
         MainActivity.ShortWeather_ArrayList.get(0).setTMN(a);
-        String test1="";
-        for(int i = 0; i< MainActivity.ShortWeather_ArrayList.size();i++){
-            test1 = test1 + "날짜 : " +MainActivity.ShortWeather_ArrayList.get(i).getFcstDate()
-                    +"\n 최저기온 : " +MainActivity.ShortWeather_ArrayList.get(i).getTMN()+
-                    "\n 최저기온 : " + MainActivity.ShortWeather_ArrayList.get(i).getTMX()+"\n";
+        String test1 = "";
+        for (int i = 0; i < MainActivity.ShortWeather_ArrayList.size(); i++) {
+            test1 = test1 + "날짜 : " + MainActivity.ShortWeather_ArrayList.get(i).getFcstDate()
+                    + "\n 최저기온 : " + MainActivity.ShortWeather_ArrayList.get(i).getTMN() +
+                    "\n 최저기온 : " + MainActivity.ShortWeather_ArrayList.get(i).getTMX() + "\n";
 
         }
-        test.setText(test1);
-
-        init();
-    }
-
-    private void init() {
-        weatherListview = findViewById(R.id.weatherListview);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
-        weatherListview.setLayoutManager(layoutManager);
-
-        ArrayList<weatherListViewItem> itemList = new ArrayList<>();
-
-        for(int i = 0; i < MainActivity.ShortWeather_ArrayList.size(); i++) {
-            String rainState = MainActivity.ShortWeather_ArrayList.get(i).getPTY();
-            String skyState = MainActivity.ShortWeather_ArrayList.get(i).getSKY();
-            String month = MainActivity.ShortWeather_ArrayList.get(i).getFcstDate().substring(4,6);
-            String date = MainActivity.ShortWeather_ArrayList.get(i).getFcstDate().substring(6);
-            Drawable skyIcon = null;
-            if(rainState.equals("1") || rainState.equals("4")) //Rain or shower
-                skyIcon = ContextCompat.getDrawable(this,R.drawable.rainicon);
-            else if(rainState.equals("2")) //Rain or Snow
-                skyIcon = ContextCompat.getDrawable(this,R.drawable.rainsnowicon);
-            else if(rainState.equals("3")) //Snow
-                skyIcon = ContextCompat.getDrawable(this,R.drawable.snowicon);
-            //else if(rainState.equals("4")) //Shower
-                //skyIcon = ContextCompat.getDrawable(this,R.drawable.showericon);
-            else if(rainState.equals("0")) { //nothing
-                if(skyState.equals("1")) //sunny
-                    skyIcon = ContextCompat.getDrawable(this,R.drawable.sunnyicon);
-                else if(skyState.equals("3")) //lots of cloud
-                    skyIcon = ContextCompat.getDrawable(this,R.drawable.lotsofcloudicon);
-                else if(skyState.equals("4")) //cloudy
-                    skyIcon = ContextCompat.getDrawable(this,R.drawable.cloudyicon);
-                else Toast.makeText(this, "skyState is " + skyState, Toast.LENGTH_SHORT).show();
-            }
-            else Toast.makeText(WeatherInfo.this, "rainState is " + rainState, Toast.LENGTH_SHORT).show();
-
-            itemList.add(new weatherListViewItem(skyIcon, month + "월 " + date + "일",
-                    MainActivity.ShortWeather_ArrayList.get(i).getTMN() + "/" + MainActivity.ShortWeather_ArrayList.get(i).getTMX()));
-        }
-
-        weatherAdapter = new myWeatherAdapter(this,itemList);
-        weatherListview.setAdapter(weatherAdapter);
-
-        myWeatherListviewDecoration decoration = new myWeatherListviewDecoration();
-        weatherListview.addItemDecoration(decoration);
     }
 }
--- app/src/main/java/com/example/coronatravel/detail/Detail_view.java
+++ app/src/main/java/com/example/coronatravel/detail/Detail_view.java
@@ -1,9 +1,13 @@
 package com.example.coronatravel.detail;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +15,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.coronatravel.DbOpenHelper;
 import com.example.coronatravel.HttpReqTask;
@@ -18,11 +23,18 @@ import com.example.coronatravel.LocationBasedList_Class;
 import com.example.coronatravel.MainActivity;
 import com.example.coronatravel.Mask;
 import com.example.coronatravel.R;
+import com.example.coronatravel.ShortWeather;
+import com.example.coronatravel.WeatherInfo;
+import com.example.coronatravel.myWeatherAdapter;
+import com.example.coronatravel.myWeatherListviewDecoration;
+import com.example.coronatravel.weatherListViewItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class Detail_view extends AppCompatActivity {
 
@@ -32,6 +44,12 @@ public class Detail_view extends AppCompatActivity {
     String addr1, contentid,contenttypeid,firstimage,title;
     LayoutInflater layoutInflater;
     LinearLayout linearLayout;
+
+    private RecyclerView weatherListview;
+    private myWeatherAdapter weatherAdapter;
+    LinearLayoutManager layoutManager;
+    ArrayList<weatherListViewItem> itemList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +60,10 @@ public class Detail_view extends AppCompatActivity {
         testMask =(TextView) findViewById(R.id.testMask);
         mDbOpenHelper = new DbOpenHelper(this);
 
-        /*layoutInflater=LayoutInflater.from(getApplicationContext());
-        View view = layoutInflater.inflate(R.layout.fragment_totalsearch,null);
-        linearLayout=findViewById(R.id.test_layout);
-        linearLayout.addView(view);*/
 
+        weatherListview = findViewById(R.id.weatherListview);
+        getdata();
+        init();
 
         Intent intent = getIntent();
         int position = intent.getExtras().getInt("position");
@@ -227,6 +244,115 @@ public class Detail_view extends AppCompatActivity {
             testMask.setText("\n\n근방 2km 이내의 마스크 판매처 중 1개\n이름 : " + MainActivity.MASK_AraayList.get(0).getName()
                     + "\n주소 : " + MainActivity.MASK_AraayList.get(0).getAddr()
                     + "\n재고 : " + MainActivity.MASK_AraayList.get(0).getRemain_stat());
+        }
+    }
+
+    public void init() {
+
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        weatherListview.setLayoutManager(layoutManager);
+
+        itemList = new ArrayList<>();
+
+        for (int i = 0; i < MainActivity.ShortWeather_ArrayList.size(); i++) {
+            String rainState = MainActivity.ShortWeather_ArrayList.get(i).getPTY();
+            String skyState = MainActivity.ShortWeather_ArrayList.get(i).getSKY();
+            String month = MainActivity.ShortWeather_ArrayList.get(i).getFcstDate().substring(4, 6);
+            String date = MainActivity.ShortWeather_ArrayList.get(i).getFcstDate().substring(6);
+            Drawable skyIcon = null;
+            if (rainState.equals("1") || rainState.equals("4")) //Rain or shower
+                skyIcon = ContextCompat.getDrawable(this, R.drawable.rainicon);
+            else if (rainState.equals("2")) //Rain or Snow
+                skyIcon = ContextCompat.getDrawable(this, R.drawable.rainsnowicon);
+            else if (rainState.equals("3")) //Snow
+                skyIcon = ContextCompat.getDrawable(this, R.drawable.snowicon);
+                //else if(rainState.equals("4")) //Shower
+                //skyIcon = ContextCompat.getDrawable(this,R.drawable.showericon);
+            else if (rainState.equals("0")) { //nothing
+                if (skyState.equals("1")) //sunny
+                    skyIcon = ContextCompat.getDrawable(this, R.drawable.sunnyicon);
+                else if (skyState.equals("3")) //lots of cloud
+                    skyIcon = ContextCompat.getDrawable(this, R.drawable.lotsofcloudicon);
+                else if (skyState.equals("4")) //cloudy
+                    skyIcon = ContextCompat.getDrawable(this, R.drawable.cloudyicon);
+                else Toast.makeText(this, "skyState is " + skyState, Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(this, "rainState is " + rainState, Toast.LENGTH_SHORT).show();
+
+            itemList.add(new weatherListViewItem(skyIcon, month + "월 " + date + "일",
+                    MainActivity.ShortWeather_ArrayList.get(i).getTMN() + "/" + MainActivity.ShortWeather_ArrayList.get(i).getTMX()));
+        }
+
+        weatherAdapter = new myWeatherAdapter(this, itemList);
+        weatherListview.setAdapter(weatherAdapter);
+
+        myWeatherListviewDecoration decoration = new myWeatherListviewDecoration();
+        weatherListview.addItemDecoration(decoration);
+    }
+
+    public void getdata() {
+        String pagenumber = "1";
+        String ShortWeatherURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=LuQHzrmd0D8xz9tDN8srTETgDoVfSeUV%2FAvFrhKX%2BtTdNMG7GJINi%2B6INCB7yMFJXXIO%2FKb7JfNeFdA%2BNmEIqA%3D%3D" +
+                "&numOfRows=50" +
+                "&pageNo=" + pagenumber +
+                "&dataType=JSON" +
+                "&base_date=20200520" +
+                "&base_time=0500" +
+                "&nx=55&ny=127";
+
+
+        String JSONFromShortWeatherURL = "a";
+        try {
+            JSONFromShortWeatherURL = new HttpReqTask().execute(ShortWeatherURL).get();
+        } catch (Exception e) {
+            Log.d("TAG", "jsonparsing error");
+        }
+        String totalCount = ShortWeather.JSONParsing(JSONFromShortWeatherURL);
+        int totalPage = (Integer.parseInt(totalCount) / 50) + 1;
+        pagenumber = String.valueOf(Integer.parseInt(pagenumber) + 1);
+
+        while (Integer.parseInt(pagenumber) <= totalPage) {
+            ShortWeatherURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=LuQHzrmd0D8xz9tDN8srTETgDoVfSeUV%2FAvFrhKX%2BtTdNMG7GJINi%2B6INCB7yMFJXXIO%2FKb7JfNeFdA%2BNmEIqA%3D%3D" +
+                    "&numOfRows=50" +
+                    "&pageNo=" + pagenumber +
+                    "&dataType=JSON" +
+                    "&base_date=20200520" +
+                    "&base_time=0500" +
+                    "&nx=55&ny=127";
+            try {
+                JSONFromShortWeatherURL = new HttpReqTask().execute(ShortWeatherURL).get();
+            } catch (Exception e) {
+                Log.d("TAG", "jsonparsing error");
+            }
+            ShortWeather.JSONParsing(JSONFromShortWeatherURL);
+            pagenumber = String.valueOf(Integer.parseInt(pagenumber) + 1);
+        }
+
+
+        String ShortWeatherURL2 = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=LuQHzrmd0D8xz9tDN8srTETgDoVfSeUV%2FAvFrhKX%2BtTdNMG7GJINi%2B6INCB7yMFJXXIO%2FKb7JfNeFdA%2BNmEIqA%3D%3D" +
+                "&numOfRows=50" +
+                "&pageNo=" + "1" +
+                "&dataType=JSON" +
+                "&base_date=20200520" +
+                "&base_time=0200" +
+                "&nx=55&ny=127";
+
+
+        String JSONFromShortWeatherURL2 = "";
+        try {
+            JSONFromShortWeatherURL2 = new HttpReqTask().execute(ShortWeatherURL2).get();
+        } catch (Exception e) {
+            Log.d("TAG", "jsonparsing error");
+        }
+
+        String a = ShortWeather.JSONParsing2(JSONFromShortWeatherURL2);
+        MainActivity.ShortWeather_ArrayList.get(0).setTMN(a);
+        String test1 = "";
+        for (int i = 0; i < MainActivity.ShortWeather_ArrayList.size(); i++) {
+            test1 = test1 + "날짜 : " + MainActivity.ShortWeather_ArrayList.get(i).getFcstDate()
+                    + "\n 최저기온 : " + MainActivity.ShortWeather_ArrayList.get(i).getTMN() +
+                    "\n 최저기온 : " + MainActivity.ShortWeather_ArrayList.get(i).getTMX() + "\n";
+
         }
     }
 
