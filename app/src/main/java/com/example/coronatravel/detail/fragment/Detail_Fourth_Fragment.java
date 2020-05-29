b155049fa7dd3c58428ee48ed3c6444f960188a4
JunBong <yg02005@naver.com>
2020-05-29 19:04:59 +0900

    .

--- app/src/main/java/com/example/coronatravel/detail/fragment/Detail_Fourth_Fragment.java
+++ app/src/main/java/com/example/coronatravel/detail/fragment/Detail_Fourth_Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -17,7 +18,7 @@ public class Detail_Fourth_Fragment extends Fragment {
     public Detail_Fourth_Fragment() {
         // Required empty public constructor
     }
-
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +26,8 @@ public class Detail_Fourth_Fragment extends Fragment {
 
 
         View view =inflater.inflate(R.layout.fragment_detail_fourth_, container, false);
+        listView=view.findViewById(R.id.fourth_listview);
+
         TextView fourth = view.findViewById(R.id.fourth);
         String a = "";
         for(int i=0;i<detailImage.Images.size();i++){
