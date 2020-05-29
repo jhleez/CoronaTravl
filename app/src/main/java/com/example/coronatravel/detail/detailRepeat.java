b200b99e0aa658f0a47eda9c970c875e8a60d147
이름 <>
2020-05-29 12:47:18 +0900

    반복정보

--- app/src/main/java/com/example/coronatravel/detail/detailRepeat.java
+++ app/src/main/java/com/example/coronatravel/detail/detailRepeat.java
@@ -1,84 +1,102 @@
-//package com.example.coronatravel.detail;
-//
-//import android.util.Log;
-//
-//import com.example.coronatravel.LocationBasedList_Class;
-//import com.example.coronatravel.MainActivity;
-//
-//import org.json.JSONArray;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-//public class detailRepeat {
-//    String infoname;
-//    String infotext;
-//
-//
-//    public detailRepeat JSONParsing(String JSONFromLocationBasedListaddr) {
-//        detailRepeat subclass = null;
-//
-//        String totalcount;
-//
-//        try {
-//            JSONObject jsonObject = new JSONObject(JSONFromLocationBasedListaddr);
-//            String response = jsonObject.getString("response");
-//            JSONObject jsonObject_response = new JSONObject(response);
-//
-//            String body = jsonObject_response.getString("body");
-//            JSONObject jsonObject_body = new JSONObject(body);
-//
-//            totalcount = jsonObject_body.getString("totalCount");
-//
-//            String items = jsonObject_body.getString("items");
-//            JSONObject jsonObject_items = new JSONObject(items);
-//
-//            String item = jsonObject_items.getString("item");
-//            if(totalcount.equals("1")){
-//                Log.d("tag","검색결과가 1개일때");
-//                JSONObject jsonObject_item = new JSONObject(item);
-//                try{
-//                    infoname = jsonObject_item.getString("infoname");
-//                }catch (JSONException e){
-//                    infoname="";
-//                }
-//                try{
-//                    infotext = jsonObject_item.getString("infotext");
-//                }catch (JSONException e){
-//                    infotext="";
-//                }
-//                contentid =jsonObject_item.getString("contentid");
-//                contenttypeid = jsonObject_item.getString("contenttypeid");
-//                title = jsonObject_item.getString("title");
-//
-//                LocationBasedList_Class subclass = new LocationBasedList_Class(addr1,contentid,contenttypeid,firstimage,title);
-//                MainActivity.LocationBasedList_ArrayList.add(subclass);
-//            }
-//            else {
-//                JSONArray jsonArray_item = new JSONArray(item);
-//                Log.d("tag","검색결과가 여러개일때");
-//                for (int i = 0; i < jsonArray_item.length(); i++) {
-//                    JSONObject subJsonObject = jsonArray_item.getJSONObject(i);
-//                    try {
-//                        addr1 = subJsonObject.getString("addr1");
-//                    } catch (JSONException e) {
-//                        addr1 = "";
-//                    }
-//
-//                    try {
-//                        firstimage = subJsonObject.getString("firstimage");
-//                    } catch (JSONException e) {
-//                        firstimage = "";
-//                    }
-//                    contentid = subJsonObject.getString("contentid");
-//                    contenttypeid = subJsonObject.getString("contenttypeid");
-//                    title = subJsonObject.getString("title");
-//
-//                    LocationBasedList_Class subclass = new LocationBasedList_Class(addr1, contentid, contenttypeid, firstimage, title);
-//                    MainActivity.LocationBasedList_ArrayList.add(subclass);
-//                }
-//            }
-//        } catch (JSONException e) {
-//            Log.d("TAG","parsing error");
-//        }
-//    }
-//}
+package com.example.coronatravel.detail;
+
+import android.text.Html;
+import android.util.Log;
+
+import com.example.coronatravel.LocationBasedList_Class;
+import com.example.coronatravel.MainActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class detailRepeat {
+    public detailRepeat() {
+    }
+
+    public detailRepeat(String infoname, String infotext) {
+        this.infoname = infoname;
+        this.infotext = infotext;
+    }
+
+    String infoname;
+    String infotext;
+
+    public String getInfoname() {
+        return infoname;
+    }
+
+    public void setInfoname(String infoname) {
+        this.infoname = infoname;
+    }
+
+    public String getInfotext() {
+        return infotext;
+    }
+
+    public void setInfotext(String infotext) {
+        this.infotext = infotext;
+    }
+
+    public detailRepeat JSONParsing(String JSONFromLocationBasedListaddr) {
+        String removehtml = Html.fromHtml(JSONFromLocationBasedListaddr).toString();
+        detailRepeat subclass = null;
+        String totalcount;
+        String infoname="";
+        String infotext="";
+
+        try {
+            JSONObject jsonObject = new JSONObject(removehtml);
+            String response = jsonObject.getString("response");
+            JSONObject jsonObject_response = new JSONObject(response);
+
+            String body = jsonObject_response.getString("body");
+            JSONObject jsonObject_body = new JSONObject(body);
+
+            totalcount = jsonObject_body.getString("totalCount");
+
+            String items = jsonObject_body.getString("items");
+            JSONObject jsonObject_items = new JSONObject(items);
+
+            String item = jsonObject_items.getString("item");
+            if(totalcount.equals("1")){
+                JSONObject jsonObject_item = new JSONObject(item);
+                try{
+                    infoname = jsonObject_item.getString("infoname");
+                }catch (JSONException e){
+                    infoname="";
+                }
+                try{
+                    infotext = jsonObject_item.getString("infotext");
+                }catch (JSONException e){
+                    infotext="";
+                }
+
+                subclass = new detailRepeat(infoname,infotext);
+
+            }
+            else {
+                JSONArray jsonArray_item = new JSONArray(item);
+
+                for (int i = 0; i < jsonArray_item.length(); i++) {
+                    JSONObject subJsonObject = jsonArray_item.getJSONObject(i);
+                    try{
+                        infoname = subJsonObject.getString("infoname");
+                    }catch (JSONException e){
+                        infoname="";
+                    }
+                    try{
+                        infotext = subJsonObject.getString("infotext");
+                    }catch (JSONException e){
+                        infotext="";
+                    }
+
+                    subclass = new detailRepeat(infoname,infotext);
+                }
+            }
+        } catch (JSONException e) {
+            Log.d("TAG","parsing error");
+        }
+        return subclass;
+    }
+}
