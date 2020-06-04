48ad88a45a31761438ee50a517dd642c3ef04e79
JunBong <yg02005@naver.com>
2020-06-05 00:23:38 +0900

    detailView 두번째 디자인 완료

--- app/src/main/java/com/example/coronatravel/detail/fragment/Detail_Second_Fragment.java
+++ app/src/main/java/com/example/coronatravel/detail/fragment/Detail_Second_Fragment.java
@@ -35,7 +35,17 @@ public class Detail_Second_Fragment extends Fragment {
     String contentid;
     TextView second;
 
+    LinearLayout detail12, detail14, detail15, detail25, detail28, detail32, detail38, detail39;
+
     TextView Babyseat12, Car12, Pet12, Experience12, People12, Age12, Openday12, Useingdays12, Break12, Useingtime12, Introduce12;
+    TextView Babyseat14, Car14, Pet14, People14, Taketime14, Discount14, Break14, Introduce14;
+    TextView Age15, Place15, Start15, End15, Program15, Positionintroduce15, Festival15, Reservation15, Discount15, Usemoney15;
+    TextView Distance25, Taketime25;
+    TextView Babyseat28, Age28, People28, Openday28, Usingtime28, Break28, Inputmoney28, Reservation28, Introduce28;
+    TextView Intime32, Outtime32, Pickup32, Reservation32, payback32, Introduce32;
+    TextView Babyseat38, Pet38, Card38, Jangday38, Openday38, Break38, Store38, Sellproduct38, Introduce38;
+    TextView Representation39, Addtional39, Play39, Package39, Card39, Opentime39, Openday39, Introduce39;
+
 
     public Detail_Second_Fragment() {
         // Required empty public constructor
@@ -59,6 +69,15 @@ public class Detail_Second_Fragment extends Fragment {
         contentid = Data.contentid;
         contenttypeid = Data.contenttypeid;
 
+        detail12 = view.findViewById(R.id.detail_12);
+        detail14 = view.findViewById(R.id.detail_14);
+        detail15 = view.findViewById(R.id.detail_15);
+        detail25 = view.findViewById(R.id.detail_25);
+        detail28 = view.findViewById(R.id.detail_28);
+        detail32 = view.findViewById(R.id.detail_32);
+        detail38 = view.findViewById(R.id.detail_38);
+        detail39 = view.findViewById(R.id.detail_39);
+
         Babyseat12 = view.findViewById(R.id.detail_12_babyseat);
         Car12 = view.findViewById(R.id.detail_12_car);
         Pet12 = view.findViewById(R.id.detail_12_pet);
@@ -71,80 +90,189 @@ public class Detail_Second_Fragment extends Fragment {
         Useingtime12 = view.findViewById(R.id.detail_12_usingtime);
         Introduce12 = view.findViewById(R.id.detail_12_introduce);
 
+        Babyseat14 = view.findViewById(R.id.detail_14_babyseat);
+        Car14 = view.findViewById(R.id.detail_14_car);
+        Pet14 = view.findViewById(R.id.detail_14_pet);
+        People14 = view.findViewById(R.id.detail_14_people);
+        Taketime14 = view.findViewById(R.id.detail_14_taketime);
+        Discount14 = view.findViewById(R.id.detail_14_discount);
+        Break14 = view.findViewById(R.id.detail_14_breakday);
+        Introduce14 = view.findViewById(R.id.detail_14_introduce);
+
+        Age15 = view.findViewById(R.id.detail_15_age);
+        Place15 = view.findViewById(R.id.detail_15_place);
+        Start15 = view.findViewById(R.id.detail_15_start);
+        End15 = view.findViewById(R.id.detail_15_end);
+        Program15 = view.findViewById(R.id.detail_15_program);
+        Positionintroduce15 = view.findViewById(R.id.detail_15_positionintroduce);
+        Festival15 = view.findViewById(R.id.detail_15_fetivaltime);
+        Reservation15 = view.findViewById(R.id.detail_15_reservation);
+        Discount15 = view.findViewById(R.id.detail_15_discount);
+        Usemoney15 = view.findViewById(R.id.detail_15_usemoney);
+
+        Distance25 = view.findViewById(R.id.detail_25_distance);
+        Taketime25 = view.findViewById(R.id.detail_25_taketime);
+
+
+        Babyseat28 = view.findViewById(R.id.detail_28_babyseat);
+        Age28 = view.findViewById(R.id.detail_28_age);
+        People28 = view.findViewById(R.id.detail_28_people);
+        Openday28 = view.findViewById(R.id.detail_28_openday);
+        Usingtime28 = view.findViewById(R.id.detail_28_usingtime);
+        Break28 = view.findViewById(R.id.detail_28_breakday);
+        Inputmoney28 = view.findViewById(R.id.detail_28_inputmoney);
+        Reservation28 = view.findViewById(R.id.detail_28_reservation);
+        Introduce28 = view.findViewById(R.id.detail_28_introduce);
+
+
+        Intime32 = view.findViewById(R.id.detail_32_intime);
+        Outtime32 = view.findViewById(R.id.detail_32_outtime);
+        Pickup32 = view.findViewById(R.id.detail_32_pickup);
+        Reservation32 = view.findViewById(R.id.detail_32_reservation);
+        payback32 = view.findViewById(R.id.detail_32_payback);
+        Introduce32 = view.findViewById(R.id.detail_32_introuduce);
+
+        Babyseat38 = view.findViewById(R.id.detail_38_babyseat);
+        Pet38 = view.findViewById(R.id.detail_38_pet);
+        Card38 = view.findViewById(R.id.detail_38_card);
+        Jangday38 = view.findViewById(R.id.detail_38_jangday);
+        Openday38 = view.findViewById(R.id.detail_38_opendays);
+        Break38 = view.findViewById(R.id.detail_38_breakday);
+        Store38 = view.findViewById(R.id.detail_38_storeintroduce);
+        Sellproduct38 = view.findViewById(R.id.detail_38_sellproduct);
+        Introduce38 = view.findViewById(R.id.detail_38_introduce);
+
+        Representation39 = view.findViewById(R.id.detail_39_represent);
+        Addtional39 = view.findViewById(R.id.detail_39_addition);
+        Play39 = view.findViewById(R.id.detail_39_play);
+        Package39 = view.findViewById(R.id.detail_39_package);
+        Card39 = view.findViewById(R.id.detail_39_card);
+        Opentime39 = view.findViewById(R.id.detail_39_opentime);
+        Openday39 = view.findViewById(R.id.detail_39_opendays);
+        Introduce39 = view.findViewById(R.id.detail_39_introduce);
+
+
         second = view.findViewById(R.id.second);
         second.setText("");
         if (contenttypeid.equals("12")) {
-            Babyseat12.setText(""+judge(detail_I_12.getChkbabycarriage()));
-            Car12.setText(""+judge(detail_I_12.getParking()));
-            Pet12.setText(""+judge(detail_I_12.getChkpet()));
-            Experience12.setText(""+judge(detail_I_12.getExpguide()));
-            People12.setText(""+judge(detail_I_12.getAccomcount()));
-            Age12.setText(""+judge(detail_I_12.getExpguide()));
-            Openday12.setText(""+judge(detail_I_12.getOpendate()));
-            Break12.setText(""+judge(detail_I_12.getRestdate()));
-            Useingdays12.setText(""+judge(detail_I_12.getUseseason()));
-            Useingtime12.setText(""+judge(detail_I_12.getUsetime()));
-            Introduce12.setText(""+judge(detail_I_12.getInfocenter()));
+            detail12.setVisibility(View.VISIBLE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.GONE);
+            Babyseat12.setText(judge(detail_I_12.getChkbabycarriage()));
+            Car12.setText(judge(detail_I_12.getParking()));
+            Pet12.setText(judge(detail_I_12.getChkpet()));
+            Experience12.setText(judge(detail_I_12.getExpguide()));
+            People12.setText(judge(detail_I_12.getAccomcount()));
+            Age12.setText(judge(detail_I_12.getExpguide()));
+            Openday12.setText(judge(detail_I_12.getOpendate()));
+            Break12.setText(judge(detail_I_12.getRestdate()));
+            Useingdays12.setText(judge(detail_I_12.getUseseason()));
+            Useingtime12.setText(judge(detail_I_12.getUsetime()));
+            Introduce12.setText(judge(detail_I_12.getExpagerange()));
 
         } else if (contenttypeid.equals("14")) {
-            second.setText("유모차 대여 정보 : " + detail_I_14.getChkbabycarriageculture() +
-                    "\n주차 시설 : " + detail_I_14.getParkingculture() +
-                    "\n애완동물 동반 가능 정보 : " + detail_I_14.getChkpetculture() +
-                    "\n\n수용 인원: " + detail_I_14.getAccomcountculture() +
-                    "\n관람 소요 시간 : " + detail_I_14.getSpendtime() +
-                    "\n할인 정보 :\n" + detail_I_14.getDiscountinfo() +
-                    "\n\n쉬는 날 : " + detail_I_14.getRestdateculture() +
-                    "\n문의 및 안내 : " + detail_I_14.getInfocenterculture());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.VISIBLE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.GONE);
+            detail32.setVisibility(View.GONE);
+            detail38.setVisibility(View.GONE);
+            detail39.setVisibility(View.GONE);
+            Babyseat14.setText(judge(detail_I_14.getChkbabycarriageculture()));
+            Car14.setText(judge(detail_I_14.getParkingculture()));
+            Pet14.setText(judge(detail_I_14.getChkpetculture()));
+            People14.setText(judge(detail_I_14.getAccomcountculture()));
+            Taketime14.setText(judge(detail_I_14.getSpendtime()));
+            Discount14.setText(judge(detail_I_14.getDiscountinfo()));
+            Break14.setText(judge(detail_I_14.getRestdateculture()));
+            Introduce14.setText(judge(detail_I_14.getInfocenterculture()));
         } else if (contenttypeid.equals("15")) {
-            second.setText("관람 가능 연령 : " + detail_I_15.getAgelimit() +
-                    "\n\n행사 장소 : " + detail_I_15.getEventplace() +
-                    "\n행사 시작일: " + detail_I_15.getEventstartdate() +
-                    "\n행사 종료일: " + detail_I_15.getEventenddate() +
-                    "\n행사 프로그램: " + detail_I_15.getProgram() +
-                    "\n행사장 위치 안내 :\n" + detail_I_15.getEventplace() +
-                    "\n공연 시간 : " + detail_I_15.getPlaytime() +
-                    "\n\n예매처 : " + detail_I_15.getBookingplace() +
-                    "\n할인 정보 :\n" + detail_I_15.getDiscountinfofestival() +
-                    "\n이용 요금 :" + detail_I_15.getUsetimefestival());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.VISIBLE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.GONE);
+            detail32.setVisibility(View.GONE);
+            detail38.setVisibility(View.GONE);
+            detail39.setVisibility(View.GONE);
+            Age15.setText(judge(detail_I_15.getAgelimit()));
+            Place15.setText(judge(detail_I_15.getEventplace()));
+            Start15.setText(judge(detail_I_15.getEventstartdate()));
+            End15.setText(judge(detail_I_15.getEventenddate()));
+            Program15.setText(judge(detail_I_15.getProgram()));
+            Positionintroduce15.setText(judge(detail_I_15.getEventplace()));
+            Festival15.setText(judge(detail_I_15.getPlaytime()));
+            Reservation15.setText(judge(detail_I_15.getBookingplace()));
+            Discount15.setText(judge(detail_I_15.getDiscountinfofestival()));
+            Usemoney15.setText(judge(detail_I_15.getUsetimefestival()));
         } else if (contenttypeid.equals("25")) {
-            second.setText("\n코스 총 거리 : " + detail_I_25.getDistance() +
-                    "\n코스 총 소요 시간 : " + detail_I_25.getTaketime());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.VISIBLE);
+            detail28.setVisibility(View.GONE);
+            detail32.setVisibility(View.GONE);
+            detail38.setVisibility(View.GONE);
+            detail39.setVisibility(View.GONE);
+            Distance25.setText(judge(detail_I_25.getDistance()));
+            Taketime25.setText(judge(detail_I_25.getTaketime()));
         } else if (contenttypeid.equals("28")) {
-            second.setText("유모차 대여 정보: " + detail_I_28.getChkbabycarriageleports() +
-                    "\n체험가능 연령 : " + detail_I_28.getExpagerangeleports() +
-                    "\n\n수용 인원 : " + detail_I_28.getAccomcountleports() +
-                    "\n개장 기간 : " + detail_I_28.getOpenperiod() +
-                    "\n이용 시간 : " + detail_I_28.getUsetimeleports() +
-                    "\n쉬는 날: " + detail_I_28.getRestdateleports() +
-                    "\n\n입장료: " + detail_I_28.getUsefeeleports() +
-                    "\n예약 안내 : " + detail_I_28.getReservation() +
-                    "\n\n문의 및 안내 : " + detail_I_28.getInfocenterleports());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.VISIBLE);
+            detail32.setVisibility(View.GONE);
+            detail38.setVisibility(View.GONE);
+            detail39.setVisibility(View.GONE);
+            Babyseat28.setText(judge(detail_I_28.getChkbabycarriageleports()));
+            Age28.setText(judge(detail_I_28.getExpagerangeleports()));
+            People28.setText(judge(detail_I_28.getAccomcountleports()));
+            Openday28.setText(judge(detail_I_28.getOpenperiod()));
+            Usingtime28.setText(judge(detail_I_28.getUsetimeleports()));
+            Break28.setText(judge(detail_I_28.getRestdateleports()));
+            Inputmoney28.setText(judge(detail_I_28.getUsefeeleports()));
+            Reservation28.setText(judge(detail_I_28.getReservation()));
+            Introduce28.setText(judge(detail_I_28.getInfocenterleports()));
+
         } else if (contenttypeid.equals("32")) {
-            second.setText("입실 시간: " + detail_I_32.getCheckintime() +
-                    "\n퇴실 시간 : " + detail_I_32.getCheckouttime() +
-                    "\n픽업 여부 : " + detail_I_32.getPickup() +
-                    "\n\n예약 홈페이지: " + detail_I_32.getReservationurl() +
-                    "\n환불 규정 : " + detail_I_32.getRefundregulation() +
-                    "\n문의 및 안내 : " + detail_I_32.getInfocenterlodging());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.GONE);
+            detail32.setVisibility(View.VISIBLE);
+            detail38.setVisibility(View.GONE);
+            detail39.setVisibility(View.GONE);
+            Intime32.setText(judge(detail_I_32.getCheckintime()));
+            Outtime32.setText(judge(detail_I_32.getCheckouttime()));
+            Pickup32.setText(judge(detail_I_32.getPickup()));
+            Reservation32.setText(judge(detail_I_32.getReservationurl()));
+            payback32.setText(judge(detail_I_32.getRefundregulation()));
+            Introduce32.setText(judge(detail_I_32.getInfocenterlodging()));
+
         } else if (contenttypeid.equals("38")) {
-            second.setText("유모차 대여 정보: " + detail_I_38.getChkbabycarriageshopping() +
-                    "\n애완동물 동반 가능 여부: " + detail_I_38.getChkpetshopping() +
-                    "\n신용카드 가능 정보 : " + detail_I_38.getChkcreditcardshopping() +
-                    "\n\n장 서는 날: " + detail_I_38.getFairday() +
-                    "\n개장일 : " + detail_I_38.getOpendateshopping() +
-                    "\n쉬는 날 : " + detail_I_38.getRestdateshopping() +
-                    "\n매장 안내 : " + detail_I_38.getShopguide() +
-                    "\n판매 품목 : " + detail_I_38.getSaleitem() +
-                    "\n문의 및 안내 : " + detail_I_38.getInfocentershopping());
-        } else if (contenttypeid.equals("38")) {
-            second.setText("유모차 대여 정보: " + detail_I_38.getChkbabycarriageshopping() +
-                    "\n애완동물 동반 가능 여부: " + detail_I_38.getChkpetshopping() +
-                    "\n신용카드 가능 정보 : " + detail_I_38.getChkcreditcardshopping() +
-                    "\n\n장 서는 날: " + detail_I_38.getFairday() +
-                    "\n쉬는 날 : " + detail_I_38.getRestdateshopping() +
-                    "\n매장 안내 : " + detail_I_38.getShopguide() +
-                    "\n\n판매 품목 : " + detail_I_38.getSaleitem() +
-                    "\n문의 및 안내 : " + detail_I_38.getInfocentershopping());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.GONE);
+            detail32.setVisibility(View.GONE);
+            detail38.setVisibility(View.VISIBLE);
+            detail39.setVisibility(View.GONE);
+            Babyseat38.setText(judge(detail_I_38.getChkbabycarriageshopping()));
+            Pet38.setText(judge(detail_I_38.getChkpetshopping()));
+            Card38.setText(judge(detail_I_38.getChkcreditcardshopping()));
+            Jangday38.setText(judge(detail_I_38.getFairday()));
+            Openday38.setText(judge(detail_I_38.getOpendateshopping()));
+            Break38.setText(judge(detail_I_38.getRestdateshopping()));
+            Store38.setText(judge(detail_I_38.getShopguide()));
+            Sellproduct38.setText(judge(detail_I_38.getSaleitem()));
+            Introduce38.setText(judge(detail_I_38.getInfocentershopping()));
+
         } else if (contenttypeid.equals("39")) {
             String kids = detail_I_39.getKidsfacility();
             if (kids.equals("0")) {
@@ -152,33 +280,30 @@ public class Detail_Second_Fragment extends Fragment {
             } else {
                 kids = "있음";
             }
-            second.setText("대표 메뉴: " + detail_I_39.getFirstmenu() +
-                    "\n취급 메뉴: " + detail_I_39.getTreatmenu() +
-                    "\n어린이 놀이방 여부: " + kids +
-                    "\n포장가능 여부 : " + detail_I_39.getPacking() +
-                    "\n신용카드 가능 여부: " + detail_I_39.getChkcreditcardfood() +
-                    "\n영업시간 : " + detail_I_39.getOpentimefood() +
-                    "\n영업일 : " + detail_I_39.getOpendatefood() +
-                    "\n문의 및 안내 : " + detail_I_39.getInfocenterfood());
+            detail12.setVisibility(View.GONE);
+            detail14.setVisibility(View.GONE);
+            detail15.setVisibility(View.GONE);
+            detail25.setVisibility(View.GONE);
+            detail28.setVisibility(View.GONE);
+            detail32.setVisibility(View.GONE);
+            detail38.setVisibility(View.GONE);
+            detail39.setVisibility(View.VISIBLE);
+            Representation39.setText(judge(detail_I_39.getFirstmenu()));
+            Addtional39.setText(judge(detail_I_39.getTreatmenu()));
+            Play39.setText(kids);
+            Package39.setText(judge(detail_I_39.getPacking()));
+            Card39.setText(judge(detail_I_39.getChkcreditcardfood()));
+            Opentime39.setText(judge(detail_I_39.getOpentimefood()));
+            Openday39.setText(judge(detail_I_39.getOpendatefood()));
+            Introduce39.setText(judge(detail_I_39.getInfocenterfood()));
+
         }
         return view;
     }
 
-    void init() {
-        Babyseat12 = getActivity().findViewById(R.id.detail_12_babyseat);
-        Car12 = getActivity().findViewById(R.id.detail_12_car);
-        Pet12 = getActivity().findViewById(R.id.detail_12_pet);
-        Experience12 = getActivity().findViewById(R.id.detail_12_experience);
-        People12 = getActivity().findViewById(R.id.detail_12_people);
-        Age12 = getActivity().findViewById(R.id.detail_12_age);
-        Openday12 = getActivity().findViewById(R.id.detail_12_openday);
-        Useingdays12 = getActivity().findViewById(R.id.detail_12_useingdays);
-        Break12 = getActivity().findViewById(R.id.detail_12_breakday);
-        Useingtime12 = getActivity().findViewById(R.id.detail_12_usingtime);
-        Introduce12 = getActivity().findViewById(R.id.detail_12_introduce);
-    }
+
     private String judge(String data) {
-        if(data.length()==0) return " - ";
+        if (data.length() == 0) return " - ";
         return data;
     }
 }
